ec02e55352a96193663f421ddb95030bd81c0521
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-07-30 21:36:33 +0200

    Add HMR for css

--- src/index.js
+++ src/index.js
@@ -2,4 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+if (module.hot) {
+  const hotEmitter = require('webpack/hot/emitter');
+  const DEAD_CSS_TIMEOUT = 2000;
+
+  hotEmitter.on('webpackHotUpdate', function (currentHash) {
+    document.querySelectorAll('link[href][rel=stylesheet]').forEach((link) => {
+      const nextStyleHref = link.href.replace(/(\?\d+)?$/, `?${Date.now()}`);
+      const newLink = link.cloneNode();
+      newLink.href = nextStyleHref;
+
+      link.parentNode.appendChild(newLink);
+      setTimeout(() => {
+        link.parentNode.removeChild(link);
+      }, DEAD_CSS_TIMEOUT);
+    });
+  });
+}
 ReactDOM.render(<App />, document.getElementById('app'));
