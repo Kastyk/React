5072d92dd1163d4c10da40c777e60fca5f051227
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-07-31 20:10:39 +0200

    Import and use components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './main.css';
+import JSX from './components/simpleJSX';
+import Description from './components/statelessFunctionalComponent';
+import List from './components/reactComponent';
+import ArrayProps from './components/arrayProps';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -7,7 +11,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1 className="h1">Hello World</h1>
+        {JSX}
+        <Description name="Pastek" />
+        <List />
+        <ArrayProps />
       </div>
     );
   }
