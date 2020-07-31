4456cbf3d5aee13b8be5ba3e01d7ef40043f09a9
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-07-31 20:13:47 +0200

    Add reactComponent file

---
+++ src/components/reactComponent.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Taka to lista Wam powiem</h2>
+        <ul>
+          <li>Oj</li>
+          <li>tak</li>
+          <li>tak</li>
+          <li>byczq</li>
+          <li>+1</li>
+        </ul>
+      </div>
+    );
+  }
+}
+export default List;
