2b316abba19d327c6e8bbd1d68d10d115f5dc1bc
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-08-03 15:51:04 +0200

    Add controlledInput file

---
+++ src/components/controlledInput.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class ControlledInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <input value={this.state.input} onChange={this.handleChange} />
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    );
+  }
+}
+export default ControlledInput;
