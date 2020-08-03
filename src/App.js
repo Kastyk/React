af2a48a6985e6450f56c791f118fea9dbe818e8a
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-08-03 15:49:02 +0200

    Import and add new components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import JSX from './components/simpleJSX';
 import Description from './components/statelessFunctionalComponent';
 import List from './components/reactComponent';
 import ArrayProps from './components/arrayProps';
+import Stateful from './components/statefulComponent';
+import Counter from './components/counter';
+import ControlledInput from './components/controlledInput';
+import ControlledForm from './components/controlledForm';
+import JustAnApp from './components/stateAsPropsChild';
+import CallbackProps from './components/callbackProps';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,9 +18,15 @@ class App extends Component {
     return (
       <div>
         {JSX}
-        <Description name="Pastek" />
+        <Description name="Kamil" />
         <List />
         <ArrayProps />
+        <Stateful />
+        <Counter multipier={2} />
+        <ControlledInput />
+        <ControlledForm />
+        <JustAnApp />
+        <CallbackProps />
       </div>
     );
   }
