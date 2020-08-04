201c01289f5d3af6bd705127e74ca2a46585657a
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-08-04 19:02:12 +0200

    Add inlineStyles file

---
+++ src/components/inlineStyles.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class InlineStyles extends Component {
+  render() {
+    return <div style={{ color: 'red', fontSize: 72 }}>Big Red</div>;
+  }
+}
+export default InlineStyles;
