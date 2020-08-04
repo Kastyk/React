f55a7faf027e929eb9ab6d6a6f7e9ad7546c7779
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-08-04 19:07:24 +0200

    Import and add new components

--- src/App.js
+++ src/App.js
@@ -10,6 +10,20 @@ import ControlledInput from './components/controlledInput';
 import ControlledForm from './components/controlledForm';
 import JustAnApp from './components/stateAsPropsChild';
 import CallbackProps from './components/callbackProps';
+import DidMount from './components/didMount';
+import EventListener from './components/eventListener';
+import ShouldUpdate from './components/shouldUpdate';
+import InlineStyles from './components/inlineStyles';
+import VariableStyles from './components/variableStyles';
+import AdvancedRender from './components/advancedRender';
+import IfElseRender from './components/ifElseRender';
+import ConciseConditional from './components/conciseConditional';
+import PropsRenderConditionally from './components/propsRenderConditionally';
+import ChangeCssConditionally from './components/changeCssConditionally';
+import ArrayMapRender from './components/arrayMapRender';
+import KeyAttribute from './components/keyAttribute';
+import ArrayFilter from './components/arrayFilter';
+import RenderToString from './RenderToString';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -27,6 +41,20 @@ class App extends Component {
         <ControlledForm />
         <JustAnApp />
         <CallbackProps />
+        <DidMount />
+        <EventListener />
+        <ShouldUpdate />
+        <InlineStyles />
+        <VariableStyles />
+        <AdvancedRender />
+        <IfElseRender />
+        <ConciseConditional />
+        <PropsRenderConditionally />
+        <ChangeCssConditionally />
+        <ArrayMapRender />
+        <KeyAttribute />
+        <ArrayFilter />
+        <RenderToString />
       </div>
     );
   }
