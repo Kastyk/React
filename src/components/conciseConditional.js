9ec5cc20c92a83325ed3debe0c802ded704a2532
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-08-04 19:03:24 +0200

    Add conciseConditional file

---
+++ src/components/conciseConditional.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class ConciseConditional extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true,
+    };
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState({
+      display: !this.state.display,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.toggleDisplay}>Toggle Display</button>
+        {this.state.display && <h1>Displayed!</h1>}
+      </div>
+    );
+  }
+}
+export default ConciseConditional;
