351962f347688a0200863025ac5603685ab015e0
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-08-04 19:02:35 +0200

    Add ifElseRender file

---
+++ src/components/ifElseRender.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class IfElseRender extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true,
+    };
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState({
+      display: !this.state.display,
+    });
+  }
+  render() {
+    if (this.state.display) {
+      return (
+        <div>
+          <button onClick={this.toggleDisplay}>Toggle Display</button>
+          <h1>Displayed!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleDisplay}>Toggle Display</button>
+        </div>
+      );
+    }
+  }
+}
+export default IfElseRender;
