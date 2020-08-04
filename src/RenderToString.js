654e0c1103ee68d534a7bd2845ff13aa69aaeeb8
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-08-04 19:05:53 +0200

    Add RenderToString file

---
+++ src/RenderToString.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class RenderToString extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Render From Server!</h1>
+      </div>
+    );
+  }
+}
+export default RenderToString;
